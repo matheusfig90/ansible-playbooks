8c392b80a4fb734bc904ce17d86d7126f8843e0c
Matheus Figueredo <matheus.figueredo@happycar.de>
2020-09-15 21:58:11 +0200

    Change the project structure to be more clear

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -17,5 +17,14 @@ echo "Setting up environments"
 
 cp ansible.cfg $HOME/.ansible.cfg
 
-ansible-playbook -i ./hosts ./personal.yml
-ansible-playbook -i ./hosts ./development.local.yml
+ansible-playbook -i ./hosts ./profiles/personal.yml
+
+read -e -p "Do you want to setup the development env (y/n)? " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    ansible-playbook -i ./hosts ./profiles/development.yml
+fi
+
+read -e -p "Do you want to setup the workstation env (y/n? " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    ansible-playbook -i ./hosts ./profiles/workstation.yml
+fi
