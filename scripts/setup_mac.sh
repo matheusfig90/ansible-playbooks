2d311b59971aafa532dfcc5bec13f88e098e3f4a
Matheus Figueredo <matheus.figueredo@gmail.com>
2022-03-13 20:45:43 +0100

    Fixes after installing it in M1

--- scripts/setup_mac.sh
+++ scripts/setup_mac.sh
@@ -3,6 +3,9 @@ echo "Installing requirements for Mac OS systems"
 if ! [ -x "$(command -v brew)" ]; then
     echo "Brew not installed, installing it."
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+    echo 'eval $(/opt/homebrew/bin/brew shellenv)' >> ~/.zprofile
+    eval $(/opt/homebrew/bin/brew shellenv)
 else
     echo "Brew installed, updating and upgrading it."
     brew update
